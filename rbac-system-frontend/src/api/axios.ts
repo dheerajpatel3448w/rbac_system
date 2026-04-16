990445b013036206a8a639c207f97ff579a589c5
dheerajpatel3448w <dheerajdominetar3448@gmail.com>
2026-04-16 09:38:55 +0530

    feat: initialize project gitignore and configure axios instance with auth interceptors

--- rbac-system-frontend/src/api/axios.ts
+++ rbac-system-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '@/store/auth.store';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5000/api/v1';
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1` || `http://localhost:5000/api/v1`;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
